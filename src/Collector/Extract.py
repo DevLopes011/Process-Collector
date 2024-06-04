fa427ee62f38e48fd8bb6850bc0ea03ffdcf80b1
devlopes011 <joao.arl.99@gmail.com>
2024-06-04 10:37:50 -0300

    feat: Alteração Feita a alteração do padrão de cnj.

--- src/Collector/Extract.py
+++ src/Collector/Extract.py
@@ -5,7 +5,7 @@ class Extract:
         pass
 
     def extractor_cnj (self, data):
-        results = re.findall( r'\d{7}\-\d{2}\.\d{4}.\d{2}.\d{4}', data)
+        results = re.findall( r'\b\d{7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}\b', data)
         return results
 
 
